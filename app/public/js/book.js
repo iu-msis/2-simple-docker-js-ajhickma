1ff954ddbacfc269c57a3d85b0b941c2cae63481
Lucy J Hickman <ajhickma@iu.edu>
2021-10-20 11:18:47 -0400

    Add new books
    created a form to add new books.  edit and delete buttons do not work. To delete/ edit must open workbench.

--- app/public/js/book.js
+++ app/public/js/book.js
@@ -1,28 +1,20 @@
 const bookrec = {
     data() {
         return {
-            books: []
+            
         }
     },
     computed: {},
     methods: {
-
+        prettyData(d) {
+            return dayjs(d)
+            .format('D MMM YYYY')
+        },
         prettyDollar(n) {
             const d = new Intl.NumberFormat("en-US").format(n);
             return "$ " + d;
         },
-        fetchBookData() {
-            fetch('/api/book/book.php')
-                .then(response => response.json())
-                .then((response) => {
-                    // print statment viewed in inspect 
-                    this.books= response;
-                    console.log("this.books");
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
-        }
+        
     },
     created() {
         this.fetchBookData();
--- app/public/js/index.js
+++ app/public/js/index.js
@@ -16,6 +16,8 @@ const person = {
                     large: ""
                 },
             },
+            books: [],
+            bookForm: {},
             // list: [5,6,7,8],
             message: "Waiting ...",
 
@@ -36,18 +38,51 @@ const person = {
             fetch('https://randomuser.me/api/')
                 .then(response => response.json())
                 .then((json) => {
-                    // print statment viewed in inspect 
-                    console.log("Got json back:", json);
+                    // print statment viewed in inspect
                     this.result = json.results[0];
-                    console.log("C");
                 })
                 .catch((error) => {
                     console.error(error);
                 });
+        },
+        fetchBookData() {
+            console.log("Here!")
+            fetch('/api/book/book.php')
+                .then(response => response.json())
+                .then((response) => {
+                    // print statment viewed in inspect 
+                    this.books = response;
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        },
+        postNewBook(evt) {
+            // this.bookForm.id = this.selectedBook.id;
+            // console.log("Posting!", this.bookForm);
+            // alert("Posted!");
+
+            fetch('/api/book/create.php', {
+                method:'POST',
+                body: JSON.stringify(this.bookForm),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json =>{ 
+                console.log("Returned from post:");
+                // TODO: test a result was returned!
+                this.books = json;
+                
+                // reset the form
+                this.bookForm = {};
+              });
         }
     },
     created() {
         this.fetchUserData();
+        this.fetchBookData();
     }
 
 }
